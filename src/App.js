aecc49de74df74c7c10a2b6e987a58b0d9bcbb1a
Fellipe Santana <fellipe.santana@gmail.com>
2020-08-20 13:07:28 -0300

    Implementando desafio conceitos de react js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,73 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
-  }
 
-  async function handleRemoveRepository(id) {
-    // TODO
-  }
+    const [repositories, setRepositories] = useState([]);
 
-  return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+    useEffect(() => {
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+        api
+            .get('/repositories')
+            .then(response => setRepositories(response.data))
+            .catch(err => console.log(err));
+    }, []);
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
+    async function handleAddRepository() {
+        
+        const data = {
+            title: `Desafio React JS ${Date.now()}`,
+            url: "https://github.com/fellipesantana/gostack-conceitos-reactjs",
+            techs: ["React", "ReactJS", "webpack", "babel", "Javascript"]
+        };
+
+        const response = await api.post('/repositories', data);
+        const repository = response.data;
+
+        setRepositories([...repositories, repository]);
+    }
+
+    async function handleRemoveRepository(id) {
+
+        const response = await api.delete(`/repositories/${id}`);
+        
+        if (response.status === 204) {
+
+            const index = repositories.findIndex(repository => repository.id === id);
+            const newRepositories = [...repositories];
+            newRepositories.splice(index, 1);
+
+            setRepositories(newRepositories);
+        } else {
+
+            console.log(response);
+        }
+    }
+
+    return (
+        <div>
+            <ul data-testid="repository-list">
+                {
+                    repositories.map(repository => {
+                        
+                        return (
+                            <li key={ repository.id }>
+                                { repository.title }
+                                <button onClick={() => handleRemoveRepository( repository.id )}>
+                                    Remover
+                                </button>
+                            </li>
+                        );
+                    })
+                }
+            </ul>
+
+            <button onClick={handleAddRepository}>Adicionar</button>
+        </div>
+    );
 }
 
 export default App;
